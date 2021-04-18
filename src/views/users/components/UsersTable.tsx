46e622659aa88a834ebc713c3c43cdffdf33af88
Bravecode <contact.bravecode@gmail.com>
2021-04-18 14:57:38 +0200

    Users Table - Search Styling & Functionality

--- src/views/users/components/UsersTable.tsx
+++ src/views/users/components/UsersTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { User } from '../types/User.interface';
 import { UsersTableItem } from './UsersTableItem';
 import { UsersTableSearch } from './UsersTableSearch';
@@ -8,24 +8,41 @@ export interface UsersTableProps {
 }
 
 const UsersTable: React.FC<UsersTableProps> = ({ data }) => {
+    const [searchValue, setSearchValue] = useState('');
+
+    // Spinner
     if (!data) {
         return (
             <div>
                 Loading ...
             </div>
         );
-    }    
+    }
+
+    // Handlers
+    const handleValueChange = (value: string) => {
+        setSearchValue(value);
+    }
 
     return (
-        <div className="users">
-            <UsersTableSearch />
+        <section className="users">
+            <header className="users-header">
+                <h1 className="users-header__title">
+                    Users list
+                </h1>
+            </header>
+
+            <UsersTableSearch 
+                value={searchValue}
+                onValueChange={handleValueChange}
+            />
             
             {
-                data?.map((item) => 
+                data?.filter((item) => item.name.toLowerCase().includes(searchValue.toLocaleLowerCase())).map((item) => 
                     <UsersTableItem key={item.ID} data={item} />
                 )
             }
-        </div>
+        </section>
     );
 }
 
--- src/views/users/components/UsersTableSearch.tsx
+++ src/views/users/components/UsersTableSearch.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-const UsersTableSearch: React.FC = () => {
+export interface UsersTableSearchProps {
+    value: string;
+    onValueChange: (val: string) => void;
+}
+
+const UsersTableSearch: React.FC<UsersTableSearchProps> = ({
+    value,
+    onValueChange
+}) => {
+    // Handlers
+    const handleValueChange = (e: React.FormEvent<HTMLInputElement>) => {
+        onValueChange(e.currentTarget.value);
+    }
+
+    const handleValueReset = () => {
+        onValueChange('');
+    }
+
     return (
-        <div>
-            Search
+        <div className="users-search">
+            <input 
+                className="users-search__input"
+                onChange={handleValueChange}
+                value={value}
+                placeholder="Search by user name"
+            />
+
+            {
+                !!value.length && (
+                    <button className="users-search__reset" onClick={handleValueReset}>
+                        <i className="fas fa-times-circle" />
+                    </button>
+                )
+            }
         </div>
     );
 }
